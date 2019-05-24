a93d0dbc66d85363a3538a4c733273e727977b78
Katancia <karol.katanowski@gmail.com>
2019-05-24 09:45:20 +0200

    #3 Added panel to see orders with their status and ability to group them by status

--- src/main/java/pl/karol/foodieapp/order/OrderRepository.java
+++ src/main/java/pl/karol/foodieapp/order/OrderRepository.java
@@ -3,6 +3,9 @@ package pl.karol.foodieapp.order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findAllByStatus(OrderStatus status);
 }
--- src/main/java/pl/karol/foodieapp/order/OrderStatus.java
+++ src/main/java/pl/karol/foodieapp/order/OrderStatus.java
@@ -3,5 +3,5 @@ package pl.karol.foodieapp.order;
 public enum OrderStatus {
     NEW,
     IN_PROGRESS,
-    COMPLETE
+    COMPLETE;
 }
